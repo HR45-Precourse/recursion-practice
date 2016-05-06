26b5e32d72caba75bc16a450ad041379d0e60be6
Kris Albert Lee <krisalbert@gmail.com>
2016-05-05 17:19:11 -0700

    Add solutions for numToText

--- numToText.js
+++ numToText.js
@@ -3,9 +3,6 @@ Create a function using recursion called numToText which takes a
 string and returns a new string with all numeric numbers from the
 input string converted to their corresponding text words. You can
 assume that the numbers in the string are single digit nubmers.
-Can you implement this function in two different ways?
-One way involves creating an inner helper function (or subroutine),
-the other way does not use an inner function.
 
 Extra Credit: Have the function handle numbers of any digit size.
 
@@ -13,6 +10,84 @@ Ex: numToText("I have 5 dogs and 6 ponies"); // returns "I have five dogs and si
 
 */
 
-var numToText = function(str) {
+var numbers = {
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  0: 'zero'
+};
+
+/********************************** Solution 1 *********************************/
+
+var numToTextOne = function(str) {
+  var result = '';
+
+  if(str.length === 0) {
+    return result;
+  } else {
+    var currentChar = str.charAt(0);
+    if(numbers.hasOwnProperty(currentChar)) {
+      result += numbers[currentChar]; // same as `result = result.concat(numbers[currentChar])``
+    } else {
+      result += currentChar;
+    }
+    return result + numToTextOne(str.slice(1)); // same as `return result.concat(numToText(str.slice(1)))`
+  }
+};
+
+
+/********************************** Solution 2: Passing in the result array with ever recursive call *********************************/
+
+var numToTextTwo = function(str, result) {
+  result = result || [];
+
+  if(str.length === 0) {
+    return result.join('');
+  } else {
+    var currentChar = str.charAt(0);
+    if(numbers[currentChar]){
+      result.push(numbers[currentChar]);
+    } else {
+      result.push(currentChar);
+    }
+    return numToTextTwo(str.slice(1), result);
+  }
 
 };
+
+/********************************** Solution with inner recursive function (Not recommended) *********************************/
+
+var numToTextThree = function(str) {
+  var result = '';
+
+  var createNewString = function(string) {
+    if(string.length === 0) {
+      return;
+    } else {
+      var currentChar = string.charAt(0);
+      if(numbers.hasOwnProperty(currentChar)) {
+        result += numbers [currentChar] // same as `result = result.concat(numbers[currentChar])`
+      } else {
+        result = result.concat(currentChar);
+      }
+      return createNewString(string.slice(1));
+    }
+  }
+
+  createNewString(str);
+
+  return result;
+
+};
+
+console.log(numToTextOne("I have 5 dogs and 6 ponies"));
+console.log(numToTextTwo("I have 5 dogs and 6 ponies"));
+console.log(numToTextThree("I have 5 dogs and 6 ponies"));
+
+
